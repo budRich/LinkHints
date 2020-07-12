cb7f6e79758876ca9c3f3114b734fa3926179faf
Simon Lydell <simon.lydell@gmail.com>
2020-07-12 12:46:27 +0200

    Add globals

---
+++ @types/globals.d.ts
@@ -0,0 +1,37 @@
+declare type AnyFunction = (...args: Array<unknown>) => unknown;
+
+declare type Browser = "chrome" | "firefox";
+
+declare const BROWSER: Browser;
+
+declare const BUILD_ID: string;
+
+declare const COLOR_BADGE: string;
+declare const COLOR_GREEN: string;
+declare const COLOR_PURPLE: string;
+declare const COLOR_YELLOW: string;
+
+declare const DEFAULT_LOG_LEVEL_CONFIG: mixed;
+
+declare const DEFAULT_STORAGE_SYNC: mixed;
+
+declare const META_HOMEPAGE: string;
+declare const META_ICON: string;
+declare const META_NAME: string;
+declare const META_SLUG: string;
+declare const META_TUTORIAL: string;
+declare const META_VERSION: string;
+
+declare const PROD: boolean;
+
+declare function exportFunction(
+  AnyFunction,
+  mixed,
+  options?: { defineAs: string }
+): AnyFunction;
+
+declare function XPCNativeWrapper<T>(x: T): T;
+
+declare type HTMLFrameElement = HTMLIFrameElement;
+
+declare type SVGElement = Element;
